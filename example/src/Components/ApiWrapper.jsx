fcad02aea3251e913163007df3da3d4e3f203c55
Frontend-minjae <shin.minjae@cashwalk.io>
2024-01-30 23:19:40 +0900

    styled-components 설치

--- example/src/Components/ApiWrapper.jsx
+++ example/src/Components/ApiWrapper.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import styled,{ css } from 'styled-components'
 
 export default function ApiWrapper() {
   return (
-    <>
+    <StyledApiWrapper>
       <h1>ApiWrapper</h1>
       <p>
         This is an example of a component that uses the API.
       </p>
-    </>
+    </StyledApiWrapper>
   )
 }
 
-
+const StyledApiWrapper = styled.div`
+  h1 {
+    background-color: red;  
+  }
+  `;
